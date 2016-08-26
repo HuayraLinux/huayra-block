08bf840c7647fcb190e3c519d7fc1821ae115113
Ignacio Losiggio <iglosiggio@gmail.com>
2016-08-26 14:45:04 -0300

    Agregado el WM_CLASS

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,9 @@ function onGameOver(score) {
   $('.estadoPierde').show();
 }
 
+/* Reto: Leer esto sin querer pegarse un tiro */
+process.mainModule.exports.init(require('nwjs-hack').set_wmclass.bind(null, "huayra-block", true));
+
 $('document').ready(function onready() {
   ocultarTodos();
   $('.estadoDetenido').show();
